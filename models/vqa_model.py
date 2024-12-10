2051ac74a841bc9fe13642516f797af8fc147a6e
kosaurang <hungnguyenlq2408@gmail.com>
2024-12-10 18:00:59 +0700

    Update vqa_model.py

--- models/vqa_model.py
+++ models/vqa_model.py
@@ -8,23 +8,27 @@ class MBart_BEiT_Model(nn.Module):
     def __init__(self, config):
         super(MBart_BEiT_Model, self).__init__()
         self.config = config
-        self.vision_encoder_pixel = Vision_Encode_Pixel(config)
-        self.text_encoder = Bart_Encode_Feature(config) 
+        
+        # Chuyển model sang device ngay khi khởi tạo
+        self.vision_encoder_pixel = Vision_Encode_Pixel(config).to(GLOBAL_DEVICE)
+        self.text_encoder = Bart_Encode_Feature(config).to(GLOBAL_DEVICE)
+        self.embedding = Bart_Embedding(config).to(GLOBAL_DEVICE)
+        
         self.device = GLOBAL_DEVICE
         self.tokenizer = AutoTokenizer.from_pretrained(config.TEXT_EMBEDDING.PRETRAINED_NAME)
-        self.embedding = Bart_Embedding(config)
-
-        self.generator_args = {
+        
+        # Chuyển generator_args thành immutable
+        self.generator_args = types.MappingProxyType({
             'max_length': config.GENERATOR.MAX_LENGTH,
             'min_length': config.GENERATOR.MIN_LENGTH,
             'num_beams': config.GENERATOR.NUM_BEAMS,
             'length_penalty': config.GENERATOR.LENGTH_PENALTY,
             'no_repeat_ngram_size': config.GENERATOR.NO_REPEAT_NGRAM_SIZE,
             'early_stopping': config.GENERATOR.EARLY_STOPPING,
-        }
+        })
 
     def forward(self, questions, images, labels=None):
-        # Di chuyển dữ liệu sang device
+        # Đảm bảo dữ liệu trên đúng device
         images = images.to(self.device)
         if labels is not None:
             labels = labels.to(self.device)
@@ -32,11 +36,12 @@ class MBart_BEiT_Model(nn.Module):
         encoding_pixel = self.vision_encoder_pixel(images)
         inputs = self.text_encoder(questions, None, labels)
         inputs.update({'pixel_values': encoding_pixel})
-
+        
         if labels is not None:
             outputs = self.embedding(**inputs)
             return outputs.logits, outputs.loss
         else:
-            pred_ids = self.embedding.generate(**inputs, **self.generator_args)
+            # Chuyển generator_args thành dict thông thường
+            pred_ids = self.embedding.generate(**inputs, **dict(self.generator_args))
             pred_tokens = self.tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
             return pred_tokens
