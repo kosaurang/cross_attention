634db4baffe63db77f1b43d87f4797fccc6f9d67
kosaurang <hungnguyenlq2408@gmail.com>
2024-12-10 18:23:37 +0700

    Update vqa_model.py

--- models/vqa_model.py
+++ models/vqa_model.py
@@ -1,5 +1,4 @@
 # models/vqa_model.py
-import types
 from common_imports import *
 from utils.device_utils import GLOBAL_DEVICE, select_device
 from models.Vision_Encode_Pixel import Vision_Encode_Pixel
@@ -7,10 +6,9 @@ from models.Bart_Encode_Feature import Bart_Encode_Feature, Bart_Embedding, Bart
 
 class MBart_BEiT_Model(nn.Module):
     def __init__(self, config):
-        super(MBart_BEiT_Model, self).__init__()
+        super().__init__()  # Sửa lại cú pháp super()
         self.config = config
         
-        # Chuyển model sang device ngay khi khởi tạo
         self.vision_encoder_pixel = Vision_Encode_Pixel(config).to(GLOBAL_DEVICE)
         self.text_encoder = Bart_Encode_Feature(config).to(GLOBAL_DEVICE)
         self.embedding = Bart_Embedding(config).to(GLOBAL_DEVICE)
@@ -18,23 +16,30 @@ class MBart_BEiT_Model(nn.Module):
         self.device = GLOBAL_DEVICE
         self.tokenizer = AutoTokenizer.from_pretrained(config.TEXT_EMBEDDING.PRETRAINED_NAME)
         
-        # Chuyển generator_args thành immutable
-        self.generator_args = types.MappingProxyType({
+        self.generator_args = {
             'max_length': config.GENERATOR.MAX_LENGTH,
             'min_length': config.GENERATOR.MIN_LENGTH,
             'num_beams': config.GENERATOR.NUM_BEAMS,
             'length_penalty': config.GENERATOR.LENGTH_PENALTY,
             'no_repeat_ngram_size': config.GENERATOR.NO_REPEAT_NGRAM_SIZE,
             'early_stopping': config.GENERATOR.EARLY_STOPPING,
-        })
+        }
 
     def forward(self, questions, images, labels=None):
-        # Đảm bảo dữ liệu trên đúng device
+        # Chuyển đổi questions và labels sang tensor nếu chưa phải
+        if not isinstance(questions, torch.Tensor):
+            questions = torch.tensor(questions, device=self.device)
+        
+        if labels is not None and not isinstance(labels, torch.Tensor):
+            labels = torch.tensor(labels, device=self.device)
+        
         images = images.to(self.device)
         if labels is not None:
             labels = labels.to(self.device)
         
         encoding_pixel = self.vision_encoder_pixel(images)
+        
+        # Kiểm tra và xử lý input cho text_encoder
         inputs = self.text_encoder(questions, None, labels)
         inputs.update({'pixel_values': encoding_pixel})
         
@@ -42,7 +47,6 @@ class MBart_BEiT_Model(nn.Module):
             outputs = self.embedding(**inputs)
             return outputs.logits, outputs.loss
         else:
-            # Chuyển generator_args thành dict thông thường
-            pred_ids = self.embedding.generate(**inputs, **dict(self.generator_args))
+            pred_ids = self.embedding.generate(**inputs, **self.generator_args)
             pred_tokens = self.tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
             return pred_tokens
